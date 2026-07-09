a481746e51f370b7e09a89ad65123c6fce2f470d
Akash <akashbangera03@gmail.com>
2026-07-09 13:43:28 +0530

    Add files via upload

--- Threatgraph/App/core/config.py
+++ Threatgraph/App/core/config.py
@@ -1,1 +1,27 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    DATABASE_URL: str
+
+    NATS_URL: str
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
 
